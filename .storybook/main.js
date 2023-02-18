ca0905b0734e02c44dececa61ad0d9fa630642ac
Aaron <aaron.sc.taylor@gmail.com>
2023-02-18 04:00:15 -0500

    storybook settings change

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.mdx"
   ],
   "addons": [
     "@storybook/addon-links",
