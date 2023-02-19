f4e1a1b08452b6ca50d3e7efcfce45782b03f4f6
Aaron <aaron.sc.taylor@gmail.com>
2023-02-18 20:52:16 -0500

    style fixes

--- src/components/EquipmentCard/EquipmentCard.jsx
+++ src/components/EquipmentCard/EquipmentCard.jsx
@@ -1,9 +1,8 @@
-import { useState, useCallback } from 'react';
-
 // MUI CORE
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 
 // MUI COLORS
 import { red, blue, purple, grey } from '@material-ui/core/colors';
@@ -15,69 +14,112 @@ const equipmentCardStyles = {
 };
 
 const equipmentCardHeaderColors = {
+  blu: blue[500],
   red: red[500],
   lightGrey: grey[200],
+  textGrey: grey[400],
   grey: grey[500],
   darkGrey: grey[800],
   lightPurple: purple[300],
   darkPurple: purple[900]
 };
 
-const EquipmentCard = ({ inputData, handleClick }) => {
-  const [equipmentData, setEquipmentData] = useState(inputData);
-
-  useCallback(() => {
-    setEquipmentData(inputData);
-  }, [setEquipmentData]);
-
+const EquipmentCard = ({ formik, handleClick }) => {
+  const weaponInfo = formik.values;
+  console.log(weaponInfo);
 
   return (
-    <div>
+    <>
       <Button onClick={handleClick}>
+        {/* Container */}
         <Box
-          align
+          align='center'
           bgcolor={equipmentCardHeaderColors.darkGrey}
           borderColor={equipmentCardHeaderColors.darkGrey}
           border={equipmentCardStyles.border}
           width={equipmentCardStyles.width}
           height={equipmentCardStyles.height}
         >
+          {/* Styling Box */}
           <Box
             border={1}
             borderColor={equipmentCardHeaderColors.grey}
             borderBottom={1}
             height='99%'
-          >
-            <Box
-              bgcolor={equipmentCardHeaderColors.darkPurple}
-              color={equipmentCardHeaderColors.lightPurple}
-              borderBottom={1}
-              borderColor={equipmentCardHeaderColors.lightPurple}
-              height='8%'
-              width='100%'
-            >
-              <Box>
-                <Typography>
-                  <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '20px' }}>
-                    {equipmentData}
-                  </Box>
-                </Typography>
-              </Box>
+          > 
+            <Box>
+              <EquipmentHeader formik={weaponInfo} />
+              <EquipmentStats formik={weaponInfo} />
+              <Divider variant="middle" />
+              <EquipmentStats formik={weaponInfo} />
+              <Divider variant="middle" />
             </Box>
-
           </Box>
         </Box>
       </Button>
-    </div>
+    </>
   );
 };
 
-/**
-  * MUI COLOR LIBRARY
-  * 
-  * 
-  * 
-**/
+const EquipmentStats = ({ formik }) => {
+  const weaponBaseStats = formik.weaponBaseStats;
+  const weaponBonusStats = formik.weaponBonusStats;
 
+  return (
+    <Box
+      color={equipmentCardHeaderColors.textGrey}
+    > 
+      <Typography>
+        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px' }}>
+          Weapon Damage {weaponBaseStats.weaponDmg}
+        </Box>
+      </Typography>
+      <Typography>
+        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px' }}>
+          Move Speed {weaponBaseStats.weaponMoveSpd}
+        </Box>
+      </Typography>
+      <Typography>
+        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px', color: equipmentCardHeaderColors.blu }}>
+          +{weaponBonusStats.bonusPhysDmg} Additional Physical Damage
+        </Box>
+      </Typography>
+      <Typography>
+        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px', color: equipmentCardHeaderColors.blu }}>
+          +{weaponBonusStats.bonusMgcDmg} Additional Magical Damage
+        </Box>
+      </Typography>
+      <Typography>
+        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px', color: equipmentCardHeaderColors.blu }}>
+          +{weaponBonusStats.bonusMoveSpd} Additional Move Speed
+        </Box>
+      </Typography>
+
+
+    </Box>  
+  )
+};
+
+const EquipmentHeader = ({ formik }) => {
+  const weaponInfo = formik;
+  return (
+    <Box
+      bgcolor={equipmentCardHeaderColors.darkPurple}
+      color={equipmentCardHeaderColors.lightPurple}
+      borderBottom={1}
+      borderColor={equipmentCardHeaderColors.lightPurple}
+      width='100%'
+      pt={1}
+    >
+      <Box>
+        <Typography component={'span'}>
+          <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '23px' }}>
+            {weaponInfo.weaponName}
+          </Box>
+        </Typography>
+      </Box>
+    </Box>
+  )
+};
 
 export default EquipmentCard;
