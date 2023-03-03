a9f927a632017e6c58718c755888d97da2c2b6b5
Aaron <aaron.sc.taylor@gmail.com>
2023-03-03 05:32:57 -0500

    defaultItem fixture

---
+++ src/utility/defaultItem.js
@@ -0,0 +1,26 @@
+export const defaultItem = {
+  weaponBaseStats: {
+    weaponBaseStat1: null,
+    weaponBaseStat2: null,
+    weaponBaseStat3: null
+  },
+  weaponBaseStatNames: {
+    weaponStatName1: null,
+    weaponStatName2: null,
+    weaponStatName3: null
+  },
+  weaponBonusStats: {
+    weaponBaseStat1: null,
+    weaponBaseStat2: null,
+    weaponBaseStat3: null
+  },
+  weaponBonusStatNames: {
+    weaponStatName1: null,
+    weaponStatName2: null,
+    weaponStatName3: null
+  },
+  weaponType: null,
+  weaponName: null,
+  weaponSlotType: null,
+  weaponHandType: null
+}
