ffb2a45d5f7a0412bcac667063f9a2416c068ddb
Aaron <aaron.sc.taylor@gmail.com>
2023-03-04 10:08:00 -0500

    itemSchema changes

---
+++ src/utility/itemSchemas.js
@@ -0,0 +1,47 @@
+export const weaponSchema = {
+  weapon: null,
+  weaponType: null,
+  weaponMoveSpeedPenalty: null,
+  weaponBaseDmg: null,
+  weaponRarity: null,
+  weaponBonusStats: {
+    statTypes: {
+      statType1: null,
+      statType2: null,
+      statType3: null,
+      statType4: null,
+      statType5: null
+
+    },
+    bonuses: {
+      statBonus1: null,
+      statBonus2: null,
+      statBonus3: null,
+      statBonus4: null,
+      statBonus5: null
+    }
+  }
+};
+
+export const jewelrySchema = {
+  jewelry: null,
+  jewelryType: null,
+  jewelryRarity: null,
+  jewelryBonusStats: {
+    statTypes: {
+      statType1: null,
+      statType2: null,
+      statType3: null,
+      statType4: null,
+      statType5: null
+
+    },
+    bonuses: {
+      statBonus1: null,
+      statBonus2: null,
+      statBonus3: null,
+      statBonus4: null,
+      statBonus5: null
+    }
+  }
+};
