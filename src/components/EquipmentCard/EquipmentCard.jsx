281fefed81c1260d011b97fa60727f17fa65c87c
Aaron <aaron.sc.taylor@gmail.com>
2023-03-07 05:10:58 -0500

    eqCard headerStyles changes

--- src/components/EquipmentCard/EquipmentCard.jsx
+++ src/components/EquipmentCard/EquipmentCard.jsx
@@ -1,5 +1,5 @@
 // HOOKS
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 // UTILITIES
 import ReactCardFlip from 'react-card-flip';
 
@@ -13,23 +13,73 @@ import { red, blue, purple, grey } from '@material-ui/core/colors';
 
 ////////////////////////////////////////////////////////////////////////////
 
+const colorLibrary = {
+  blu: blue[500],
+  darkBlu: blue[900],
+  neonBlu: '#00e5ff',
+  red: red[500],
+  lightPurple: purple[300],
+  darkPurple: purple[900],
+  white: grey[50],
+  lightGrey: grey[200],
+  textGrey: grey[400],
+  grey: grey[500],
+  grey2: grey[600],
+  darkGrey: grey[800],
+  neonGreen: '#76ff03',
+  green: '#2e7d32',
+  amber: '#ffc107',
+  amberBrown: '#994d00'
+};
+
 const equipmentCardStyles = {
   width: '275px',
   height: '425px',
   border: 3
 };
 
+const headerColorStyles = {
+  common: {
+    backGroundColor: colorLibrary.grey2,
+    borderAndTextColor: colorLibrary.white
+  },
+  uncommon: {
+    backGroundColor: colorLibrary.green,
+    borderAndTextColor: colorLibrary.neonGreen
+  },
+  rare: {
+    backGroundColor: colorLibrary.darkBlu,
+    borderAndTextColor: colorLibrary.neonBlu
+  },
+  epic: {
+    backGroundColor: colorLibrary.darkPurple,
+    borderAndTextColor: colorLibrary.lightPurple
+  },
+  legendary: {
+    backGroundColor: colorLibrary.amberBrown,
+    borderAndTextColor: colorLibrary.amber
+  },
+  default: {
+    backGroundColor: colorLibrary.darkPurple,
+    borderAndTextColor: colorLibrary.lightPurple
+  },
+};
+
 const EquipmentCard = ({ formik }) => {
   const [isFlipped, setIsFlipped]= useState(false);
+  
   const handleClick = () => setIsFlipped(!isFlipped);
+  
+  
   return (
     <ReactCardFlip
     isFlipped={isFlipped}
     flipSpeedBackToFront={0.8}
     flipSpeedFrontToBack={0.8}
     >
-      <EquipmentCardFront 
+      <EquipmentCardFront
         formik={formik}
+        headerColorStyles={headerColorStyles}
         handleClick={handleClick}
       />
       <EquipmentCardBack
@@ -40,15 +90,16 @@ const EquipmentCard = ({ formik }) => {
   );
 };
 
-const EquipmentCardFront = ({ formik, handleClick }) => {
+const EquipmentCardFront = ({ formik, headerColorStyles, handleClick }) => {
   const weaponInfo = formik.values;
+  console.log(weaponInfo);
   return (
     <Button onClick={handleClick}>
       {/* Container */}
       <Box
         align='center'
-        bgcolor={equipmentCardHeaderColors.darkGrey}
-        borderColor={equipmentCardHeaderColors.darkGrey}
+        bgcolor={colorLibrary.darkGrey}
+        borderColor={colorLibrary.darkGrey}
         border={equipmentCardStyles.border}
         width={equipmentCardStyles.width}
         height={equipmentCardStyles.height}
@@ -56,12 +107,12 @@ const EquipmentCardFront = ({ formik, handleClick }) => {
         {/* Styling Box */}
         <Box
           border={1}
-          borderColor={equipmentCardHeaderColors.grey}
+          borderColor={colorLibrary.grey}
           borderBottom={1}
           height='99%'
         > 
           <Box>
-            <EquipmentHeader formik={weaponInfo} />
+            <EquipmentHeader formik={weaponInfo} headerColorStyles={headerColorStyles} />
             <EquipmentStats formik={weaponInfo} />
             <Divider variant="middle" />
             <Divider variant="middle" />
@@ -82,8 +133,8 @@ const EquipmentCardBack = ({ formik, handleClick }) => {
       {/* Container */}
       <Box
         align='center'
-        bgcolor={equipmentCardHeaderColors.darkGrey}
-        borderColor={equipmentCardHeaderColors.darkGrey}
+        bgcolor={colorLibrary.darkGrey}
+        borderColor={colorLibrary.darkGrey}
         border={equipmentCardStyles.border}
         width={equipmentCardStyles.width}
         height={equipmentCardStyles.height}
@@ -91,7 +142,7 @@ const EquipmentCardBack = ({ formik, handleClick }) => {
         {/* Styling Box */}
         <Box
           border={1}
-          borderColor={equipmentCardHeaderColors.grey}
+          borderColor={colorLibrary.grey}
           borderBottom={1}
           height='99%'
         > 
@@ -108,10 +159,10 @@ const ContactHeader = ({ formik }) => {
   const weaponInfo = formik;
   return (
     <Box
-      bgcolor={equipmentCardHeaderColors.darkPurple}
-      color={equipmentCardHeaderColors.lightPurple}
+      bgcolor={colorLibrary.darkPurple}
+      color={colorLibrary.lightPurple}
       borderBottom={1}
-      borderColor={equipmentCardHeaderColors.lightPurple}
+      borderColor={colorLibrary.lightPurple}
       width='100%'
       pt={1}
     >
@@ -129,12 +180,38 @@ const ContactHeader = ({ formik }) => {
 
 const EquipmentHeader = ({ formik }) => {
   const weaponInfo = formik;
+  const [headerColors, setHeaderColors] = useState(headerColorStyles.common);
+  const handleEquipmentCardHeaderStyle = (rarity) => {
+    switch (rarity) {
+      case 'Common':
+        setHeaderColors(headerColorStyles.common);
+        break;
+      case 'Uncommon':
+        setHeaderColors(headerColorStyles.uncommon);
+        break;
+      case 'Rare':
+        setHeaderColors(headerColorStyles.rare);
+        break;
+      case 'Epic':
+        setHeaderColors(headerColorStyles.epic);
+        break;
+      case 'Legendary':
+        setHeaderColors(headerColorStyles.legendary);
+        break;
+      default:
+        setHeaderColors(headerColorStyles.common);
+        break;
+    };
+  };
+  useEffect(() => {
+    handleEquipmentCardHeaderStyle(weaponInfo.rarity);
+  }, [handleEquipmentCardHeaderStyle, weaponInfo]);
   return (
     <Box
-      bgcolor={equipmentCardHeaderColors.darkPurple}
-      color={equipmentCardHeaderColors.lightPurple}
+      bgcolor={headerColors.backGroundColor}
+      color={headerColors.borderAndTextColor}
+      borderColor={headerColors.borderAndTextColor}
       borderBottom={1}
-      borderColor={equipmentCardHeaderColors.lightPurple}
       width='100%'
       pt={1}
     >
@@ -155,7 +232,7 @@ const EquipmentStats = ({ formik }) => {
   return (
     <Box
       sx={{ py: 0.7 }}
-      color={equipmentCardHeaderColors.textGrey}
+      color={colorLibrary.textGrey}
     > 
       <Typography component={'span'}>
         <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px' }}>
@@ -168,17 +245,17 @@ const EquipmentStats = ({ formik }) => {
         </Box>
       </Typography>
       <Typography component={'span'}>
-        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px', color: equipmentCardHeaderColors.blu }}>
+        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px', color: colorLibrary.blu }}>
           +{weaponBonusStats.bonusPhysDmg} Additional Physical Damage
         </Box>
       </Typography>
       <Typography component={'span'}>
-        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px', color: equipmentCardHeaderColors.blu }}>
+        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px', color: colorLibrary.blu }}>
           +{weaponBonusStats.bonusMgcDmg} Additional Magical Damage
         </Box>
       </Typography>
       <Typography component={'span'}>
-        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px', color: equipmentCardHeaderColors.blu }}>
+        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px', color: colorLibrary.blu }}>
           +{weaponBonusStats.bonusMoveSpd} Additional Move Speed
         </Box>
       </Typography>
@@ -209,27 +286,27 @@ const EquipmentDescription = ({ formik }) => {
   return (
     <Box sx={{ py: 0.7 }}>
       <Typography component={'span'}>
-        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px', color: equipmentCardHeaderColors.red }}>
+        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px', color: colorLibrary.red }}>
           Required Class:
         </Box>
       </Typography>
       <Typography component={'span'}>
-        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px', color: equipmentCardHeaderColors.red }}>
+        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px', color: colorLibrary.red }}>
           {weaponInfo.weaponType}
         </Box>
       </Typography>
       <Typography component={'span'}>
-        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px', color: equipmentCardHeaderColors.textGrey }}>
+        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px', color: colorLibrary.textGrey }}>
           Slot Type: {weaponInfo.weaponSlotType}
         </Box>
       </Typography>
       <Typography component={'span'}>
-        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px', color: equipmentCardHeaderColors.textGrey }}>
+        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px', color: colorLibrary.textGrey }}>
           Hand Type: {weaponInfo.weaponHandType}
         </Box>
       </Typography>
       <Typography component={'span'}>
-        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px', color: equipmentCardHeaderColors.textGrey }}>
+        <Box sx={{ textAlign: 'center', fontFamily: 'Helvetica Neue', textTransform: 'capitalize', fontSize: '17px', color: colorLibrary.textGrey }}>
           Weapon Type: {weaponInfo.weaponType}
         </Box>
       </Typography>
@@ -237,15 +314,6 @@ const EquipmentDescription = ({ formik }) => {
   );
 };
 
-const equipmentCardHeaderColors = {
-  blu: blue[500],
-  red: red[500],
-  lightGrey: grey[200],
-  textGrey: grey[400],
-  grey: grey[500],
-  darkGrey: grey[800],
-  lightPurple: purple[300],
-  darkPurple: purple[900]
-};
+
 
 export default EquipmentCard;
