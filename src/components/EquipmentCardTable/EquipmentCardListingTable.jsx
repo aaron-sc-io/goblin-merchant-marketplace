5efd0d692191c1d41f99463b76cff052e7ac947a
Aaron <aaron.sc.taylor@gmail.com>
2023-03-10 07:31:03 -0500

    listing adjs

--- src/components/EquipmentCardTable/EquipmentCardListingTable.jsx
+++ src/components/EquipmentCardTable/EquipmentCardListingTable.jsx
@@ -1,80 +1,41 @@
 import EquipmentCard from '../EquipmentCard/EquipmentCard';
 import { Grid } from '@mui/material';
 import { useFormik } from 'formik';
-
+import { weaponValues1, weaponValues2, weaponValues3 } from './listingExamples';
 
 const EquipmentCardListingTable = () => {
-  const handleClick = async() => alert('will be redirected to {weaponPage}');
-  const formik = useFormik({
-    initialValues: {
-      weaponType: 'Dagger',
-      weaponName: 'Example Dagger',
-      rarity: 'Rare',
-      weaponSlotType: 'Secondary Weapon',
-      weaponHandType: 'One-Handed',
-      baseStats: {
-        statNames: {
-          statName1: 'Weapon Damage',
-          statName2: 'Movement Speed',
-          statName3: null,
-          statName4: null
-        },
-        statValues: {
-          statValue1: 20,
-          statValue2: -10,
-          statValue3: null,
-          statValue4: null
-        }
-      },
-      bonusStats: {
-        statNames: {
-          statName1: 'All Attributes',
-          statName2: 'Movement Speed',
-          statName3: null,
-          statName4: null,
-          statName5: null
-        },
-        statValues: {
-          statValue1: 2,
-          statValue2: 2,
-          statValue3: null,
-          statValue4: null,
-          statValue5: null
-        }
-      },
-    equipmentDescription: {
-      weaponSlotType: 'Secondary Weapon',
-      weaponHandType: 'One-Handed',
-      weaponDesc: 'Info'
-    }
-  }});
+  const formik1 = useFormik({
+    initialValues: weaponValues1
+  });
+  const formik2 = useFormik({
+    initialValues: weaponValues2
+  });
+  const formik3 = useFormik({
+    initialValues: weaponValues3
+  });
   // const handleUpdate = async() => {try() catch(e) loge};
   return (
     <>
       <Grid container>
         {/* Populate with */}
         <Grid item xs={3}>
-          <EquipmentCard 
-              handleClick={handleClick}
-              formik={formik}
+          <EquipmentCard
+              formik={formik1}
             />
         </Grid>
         <Grid item xs={3}>
-          <EquipmentCard 
-              handleClick={handleClick}
-              formik={formik}
+          <EquipmentCard
+              formik={formik2}
             />
         </Grid>
         <Grid item xs={3}>
-          <EquipmentCard 
-              handleClick={handleClick}
-              formik={formik}
+          <EquipmentCard
+              formik={formik3}
             />
         </Grid>
         <Grid item xs={3}>
-          <EquipmentCard 
-              handleClick={handleClick}
-              formik={formik}
+          <EquipmentCard
+              formik={formik2}
             />
         </Grid>
       </Grid>
