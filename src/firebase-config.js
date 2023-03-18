701682cc641cea4a5e4f177830fca63bc2640e62
Aaron <aaron.sc.taylor@gmail.com>
2023-03-18 12:39:18 -0400

    fb config

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjunf9qyo_muBtfJZa1H6fWc6SrGissmw",
+  authDomain: "market-n-marketer.firebaseapp.com",
+  projectId: "market-n-marketer",
+  storageBucket: "market-n-marketer.appspot.com",
+  messagingSenderId: "96929437164",
+  appId: "1:96929437164:web:ff3ab5d1458ea0b601dcc9",
+  measurementId: "G-W4BM47XZXV"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export default app;
