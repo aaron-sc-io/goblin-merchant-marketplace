f8f3cb0ea16d2aad722a66f9e7568e9855d0e3fc
Jsmed00 <smedleyj18@students.ecu.edu>
2023-03-18 13:31:45 -0400

    SignUp card init

--- src/components/SignUpCard/SignUpCard.jsx
+++ src/components/SignUpCard/SignUpCard.jsx
@@ -1,11 +1,24 @@
 import { TextField, Box, Button } from '@mui/material';
 
-const SignUpCard = () => {
+const SignUpCard = ({ handleClick }) => {
   return(
-    <>
-      'hi'
-    </>
+    <Box sx={{ display: 'flex', flexDirection: 'column', width: '250px' }}>
+      <TextField id="filled-basic" label="Email Address" variant="filled" />
+      <TextField
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="filled"
+        />
+      <Button variant="contained" onClick={handleClick}>Sign Up</Button>
+    </Box>
   );
 };
+
+
+// toolbar on top with buttons and such with height of 75 maybe
+
+
 
 export default SignUpCard;
