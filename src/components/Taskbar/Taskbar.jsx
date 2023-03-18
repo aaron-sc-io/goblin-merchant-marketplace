9a4a9c608066780a301685c4cff1a67eb5c7620d
Jsmed00 <smedleyj18@students.ecu.edu>
2023-03-18 13:31:56 -0400

    Taskbar init

---
+++ src/components/Taskbar/Taskbar.jsx
@@ -0,0 +1,16 @@
+import { TextField, Box, Button } from '@mui/material';
+
+const Taskbar = ({}) => {
+  return(
+    <Box sx={{ display: 'flex', flexDirection: 'row', height: '75px' }}>
+      
+    </Box>
+  );
+};
+
+
+// toolbar on top with buttons and such with height of 75 maybe
+
+
+
+export default Taskbar;
