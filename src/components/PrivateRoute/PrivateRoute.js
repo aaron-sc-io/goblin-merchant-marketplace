38722abe89598fa693bf082691ff5e420ffd366d
Aaron <aaron.sc.taylor@gmail.com>
2023-04-08 02:34:44 -0400

    private Route

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,10 +1,10 @@
-import { Route, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
 
 const PrivateRoute = ({ children }) => {
   const { currentUser } = useAuth();
 
-  return currentUser ? children : <Navigate to="/signin" />;
+  return currentUser ? children : <Navigate to="/" />;
 }
 
 export default PrivateRoute;
