60f1801787f2696cbc1fadf647f8c8de1c14799c
Aaron <aaron.sc.taylor@gmail.com>
2023-04-08 02:34:14 -0400

    routing setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import { AuthProvider } from './contexts/AuthContext';
-import SignUpCard from './components/SignUpCard/SignUpCard';
-import SignInCard from './components/SignInCard/SignInCard';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
-import PrivateRoute from './components/PrivateRoute/PrivateRoute'
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+
+import { Box, Card } from '@mui/material'
+
+import SignUpCardContainer from './components/SignUpCard/SignUpCardContainer';
+import SignInCardContainer from './components/SignInCard/SignInCardContainer';
+import ItemFormContainer from './components/ItemForm/ItemFormContainer';
+import EquipmentCardListingTableContainer from './components/EquipmentCardListingTable/EquipmentCardListingTableContainer';
 
 const App = () => {
   return (
     <Router>
       <AuthProvider>
-        <Routes>
-          <Route exact path = '/'
-            element={<PrivateRoute>
-                      <Dashboard /> 
-                    </PrivateRoute>}
-            />
-          <Route exact path = '/signup' element={<SignUpCard />} />
-          <Route exact path = '/signin' element={<SignInCard />} />
-        </Routes>
+        <Box sx={{ backgroundColor: '#AAAAAA', height: '1000vh' }}>
+          <Box sx={{ justifyContent: 'center', maxWidth: '1200px', margin: 'auto', px: '10px' }}>
+            <Box sx={{ py: '10px' }}>
+              <Dashboard /> {/* TaskbarContainer */}
+            </Box>
+            <Card sx={{ backgroundColor: '#DDDDDD' }}>
+              <Routes>
+                <Route exact path = '/submit-listing'
+                  element={<PrivateRoute>
+                            <ItemFormContainer />
+                          </PrivateRoute>}
+                  />
+                <Route exact path = '/signup' element={<SignUpCardContainer />} />
+                <Route exact path = '/signin' element={<SignInCardContainer />} />
+                <Route exact path = '/password-reset' element={<SignInCardContainer />} />
+                <Route exact path = '/' element={<EquipmentCardListingTableContainer />} 
+                  />
+              </Routes>
+            </Card>
+          </Box>
+        </Box>
       </AuthProvider>
     </Router>
   )
