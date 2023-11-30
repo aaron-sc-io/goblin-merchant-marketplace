6f11bb8e1171be64ff65ba3d72eb5ca2d87e394a
Aaron <aaron.sc.taylor@gmail.com>
2023-11-29 23:14:22 -0500

    app bg fix

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ const App = () => {
   return (
     <Router>
       <AuthProvider>
-        <Box sx={{backgroundImage: `url(${background})`}}>
-          <Box sx={{ justifyContent: 'center', maxWidth: '1550px', margin: 'auto', px: '10px' }}>
+        <Box sx={{ backgroundImage: `url(${background})`, minHeight: '100vh'}}>
+          <Box sx={{ justifyContent: 'center', maxWidth: '1550px', margin: 'auto', px: '5px', minHeight: '1vh', pb: 5 }}>
             <Box sx={{ py: '10px' }}>
               <TaskbarContainer />
             </Box>
