a7c82bc9c0e3ded8de207b09e7132a2dcf15aa42
johnny <jhuang007007@gmail.com>
2023-11-30 14:45:18 -0500

    button styling

--- src/components/DndButton/DndButton.jsx
+++ src/components/DndButton/DndButton.jsx
@@ -1,14 +1,17 @@
 import './DndButton.css';
-import button_default from '../../utility/img/button_default.webp';
+import button_default from '../../utility/img/button_default_cropped.webp';
 import button_frame from '../../utility/img/button_frame.webp';
 
-const DndButton = ({ width, height, text, onClick }) => {
-
+const DndButton = ({ sizeVariant, text, onClick }) => {
   return (
-    <div class="container">
-      <img src={button_default} alt="Avatar" class="frame" />
-      <img src={button_frame} alt="Avatar" class="frameOverlay" />
-    </div>
+    <button class="button" onClick={onClick}>
+      <div class="container">
+        <div class="button-text">{text}</div>
+        <img src={button_default} alt="Avatar" class="frame" />
+        <img src={button_frame} alt="Avatar" class="frameOverlay" />
+        <div class="color-overlay"/>
+      </div>
+    </button>
   );
 };
 
