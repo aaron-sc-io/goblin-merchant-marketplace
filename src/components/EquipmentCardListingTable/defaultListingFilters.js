91df623043a120767121be2b3f03658036848cf6
Aaron <aaron.sc.taylor@gmail.com>
2023-12-01 18:28:10 -0500

    filters fixture

---
+++ src/components/EquipmentCardListingTable/defaultListingFilters.js
@@ -0,0 +1,19 @@
+// TODO
+// All Listing Object Options
+const category = [];
+const name = [];
+const requiredClass = [];
+const rarity = [];
+const slotType = [];
+const enchantments = [];
+
+const defaultListingFilters = {
+  category,
+  name,
+  requiredClass,
+  rarity,
+  slotType,
+  enchantments
+};
+
+export default defaultListingFilters;
