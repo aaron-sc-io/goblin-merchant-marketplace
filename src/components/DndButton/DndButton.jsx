49ec96cd8018c8caad9cb7390faa827fc641792a
johnny <jhuang007007@gmail.com>
2023-11-30 20:39:45 -0500

    updated dndbutton styling - added glow effect on hover and click

--- src/components/DndButton/DndButton.jsx
+++ src/components/DndButton/DndButton.jsx
@@ -1,15 +1,17 @@
 import './DndButton.css';
-import button_default from '../../utility/img/button_default_cropped.webp';
+import button_default from '../../utility/img/button_default.webp';
+import button_default_highlight from '../../utility/img/button_default_highlight.webp';
 import button_frame from '../../utility/img/button_frame.webp';
 
 const DndButton = ({ disabled, sizeVariant, text, onClick }) => {
   return (
     <button class="button" disabled={disabled} onClick={onClick}>
       <div class="container">
+        <img src={button_default_highlight} alt="Avatar" class="button-default-highlight" />
         <div class="button-text">{text}</div>
         <img src={button_default} alt="Avatar" class="frame" />
-        <img src={button_frame} alt="Avatar" class="frameOverlay" />
-        <div class="color-overlay"/>
+        <img src={button_frame} alt="Avatar" class="frame-overlay" />
+
       </div>
     </button>
   );
