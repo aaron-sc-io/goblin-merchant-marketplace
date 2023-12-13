c676579df870095eafada77e2d6dde3c16b461aa
johnny <jhuang007007@gmail.com>
2023-12-13 15:42:28 -0500

    CustomAlert - error, warning, info & success style

---
+++ src/components/Alert/CustomAlert.jsx
@@ -0,0 +1,22 @@
+import '../../utility/styles/ibarraFont.css';
+import './CustomAlert.css'
+import ErrorIcon from '@mui/icons-material/ErrorOutline';
+import WarningIcon from '@mui/icons-material/WarningAmber';
+import InfoIcon from '@mui/icons-material/InfoOutlined';
+import CheckIcon from '@mui/icons-material/CheckCircleOutlined';
+
+const CustomAlert = ({ severity, message }) => {
+  return (
+    <div class={`alert ibarra ${severity}`}>
+      <div class={`alert-container`} >
+        {severity === 'error' && <ErrorIcon/>}
+        {severity === 'warning' && <WarningIcon/>}
+        {severity === 'info' && <InfoIcon/>}
+        {severity === 'success' && <CheckIcon/>}
+      </div>
+      <div class='alert-message'>{message}</div>
+    </div>
+  );
+};
+
+export default CustomAlert;
